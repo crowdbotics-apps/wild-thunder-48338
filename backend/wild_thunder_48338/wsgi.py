d1a173be841b791016ced240d02574b697af541a
Crowdbotics <team@crowdbotics.com>
2024-06-10 14:59:47 +0000

    Initial commit

---
+++ backend/wild_thunder_48338/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for wild_thunder_48338 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "wild_thunder_48338.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wild-thunder-48338",
+  url: "https://wild-thunder-48338.botics.co",
+  api: "https://wild-thunder-48338.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
